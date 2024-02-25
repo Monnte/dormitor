66ad32bc9d001a1d08029f3a28e129027cbefd14
rizzgo <seipel.richard@gmail.com>
2024-02-25 17:21:57 +0100

    Base layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,10 @@
-import {Button} from "@nextui-org/button";
-import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
+import SidebarLayout from "./SidebarLayout";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello</Button>
-      <Card className="max-w-[400px]">
-      <CardHeader className="flex gap-3">
-        <Image
-          alt="nextui logo"
-          height={40}
-          radius="sm"
-          src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-          width={40}
-        />
-        <div className="flex flex-col">
-          <p className="text-md">NextUI</p>
-          <p className="text-small text-default-500">nextui.org</p>
-        </div>
-      </CardHeader>
-      <Divider/>
-      <CardBody>
-        <p>Make beautiful websites regardless of your design experience.</p>
-      </CardBody>
-      <Divider/>
-      <CardFooter>
-        <Link
-          isExternal
-          showAnchorIcon
-          href="https://github.com/nextui-org/nextui"
-        >
-          Visit source code on GitHub.
-        </Link>
-      </CardFooter>
-    </Card>
+    <main className="flex flex-col gap-3">
+      <div>header</div>
+      <SidebarLayout></SidebarLayout>
     </main>
   );
 }
